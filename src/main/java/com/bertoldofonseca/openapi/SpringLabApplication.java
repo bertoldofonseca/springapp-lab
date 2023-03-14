66620444155badcca93ec4a3104a52856d3dcacd
Bertoldo Fonseca <bertoldo@gmail.com>
2023-03-14 20:34:16 -0300

    chore: fixing Application run

--- src/main/java/com/bertoldofonseca/openapi/SpringLabApplication.java
+++ src/main/java/com/bertoldofonseca/openapi/SpringLabApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLabApplication {
-
   public static void main(String[] args) {
-    SpringApplication.run(OpenapiApplication.class, args);
+    SpringApplication.run(SpringLabApplication.class, args);
   }
 }
